fda7fd994ba5f8d21fb82c144666f8c7108b9c30
Michael Woolnough <michael.woolnough@gmail.com>
2017-09-25 14:06:50 +0100

    changed how the gzip header is read

--- fileserver.go
+++ fileserver.go
@@ -28,31 +28,44 @@ func FileServer(root http.FileSystem) http.Handler {
 
 // ServerHTTP implements the http.Handler interface
 func (f fileServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	for _, e := range strings.Split(r.Header.Get("Accept-Encoding"), ",") {
-		if strings.TrimSpace(e) == "gzip" {
-			p := path.Clean(r.URL.Path)
-			m := p
-			nf, err := f.root.Open(p + ".gz")
-			if strings.HasSuffix(p, "/") {
-				m += "index.html"
-				if err != nil {
-					nf, err = f.root.Open(p + ".gz")
-					p += "index.html"
-				}
-			}
-			if err == nil {
-				if ctype := mime.TypeByExtension(filepath.Ext(m)); ctype != "" {
-					s, err := nf.Stat()
-					if err != nil {
-						break
-					}
-					w.Header().Set("Content-Type", ctype)
-					w.Header().Set("Content-Length", strconv.FormatInt(s.Size(), 10))
-					w.Header().Set("Content-Encoding", "gzip")
-					r.URL.Path = p + ".gz"
-				}
+	var compressed bool
+	accepts := r.Header.Get("Accept-Encoding")
+	for {
+		p := strings.IndexByte(accepts, ',')
+		if p < 0 {
+			if strings.TrimSpace(accepts) == "gzip" {
+				compressed = true
 			}
 			break
+		}
+		if strings.TrimSpace(accepts[:p]) == "gzip" {
+			compressed = true
+			break
+		}
+		accepts = accepts[p+1:]
+	}
+	if compressed {
+		p := path.Clean(r.URL.Path)
+		m := p
+		nf, err := f.root.Open(p + ".gz")
+		if strings.HasSuffix(p, "/") {
+			m += "index.html"
+			if err != nil {
+				nf, err = f.root.Open(p + ".gz")
+				p += "index.html"
+			}
+		}
+		if err == nil {
+			if ctype := mime.TypeByExtension(filepath.Ext(m)); ctype != "" {
+				s, err := nf.Stat()
+				if err != nil {
+					break
+				}
+				w.Header().Set("Content-Type", ctype)
+				w.Header().Set("Content-Length", strconv.FormatInt(s.Size(), 10))
+				w.Header().Set("Content-Encoding", "gzip")
+				r.URL.Path = p + ".gz"
+			}
 		}
 	}
 	f.h.ServeHTTP(w, r)
